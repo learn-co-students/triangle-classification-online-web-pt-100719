5ef93c52b80495e52d8cff627da31c3af02e82c4
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-21 06:17:09 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
 class Triangle
-  # write code here
+  attr_accessor :x, :y, :z
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
+  end
+
+  def kind
+    if kosher_triangle == true && kosher_triangle2 == true 
+      puts true 
+    else 
+      raise TriangleError 
+    end 
+    
+    if x == y && y == z && x == z
+      :equilateral
+    elsif x != y && y != z && z != x
+      :scalene 
+    else 
+      :isosceles
+    end 
+  end
+  
+  def kosher_triangle 
+    x.positive? && y.positive? && z.positive?
+  end 
+  
+  def kosher_triangle2 
+    x + y > z && x + z > y && y + z > x
+  end 
 end
+
+class TriangleError < StandardError
+  def message
+    "This triangle violates triangle inequality and it is illegal"
+end
+
+a = Triangle.new(5, 5, 5)
+a.kind
