2c12399b4fc4897955e2054730f85e19e571a238
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-11-10 17:19:57 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :a, :b, :c
+  
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+  
+  def kind
+    @a, @b, @c = [@a, @b, @c].sort
+    raise TriangleError if @a <= 0 or @a + @b <= @c
+    return :equilateral if @a == @c
+    return :isosceles if @a == @b or @b == @c
+    return :scalene
+    # if @a == @b && @a == @c && @b == @c
+    #   :equilateral
+    # elsif @b==@c || @a==@b || @c==@a 
+    #   :isosceles
+    # elsif @a <= 0 || @b <= 0 || @c <= 0
+    #   raise TriangleError
+    # elsif @a >= @b+@c || @c >= @b+@a || @b >= @a+@c
+    #   raise TriangleError
+    # else 
+    #   :scalene
+    # end
+  end
+  
+  class TriangleError < StandardError
+    def message
+      "invalid"
+    end
+  end
 end
