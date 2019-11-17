2e59ca88cfb275106e4d7c17c332f1952eb40dda
twwright <twwright@gmail.com>
2019-11-17 14:55:56 -0600

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+	attr_accessor :x, :y, :z
+
+	def initialize(x,y,z)
+		@x, @y, @z = x, y, z
+	end
+
+	def kind
+		self.valid?
+		if x != y && y != z && x != z
+			:scalene
+		elsif x == y && y == z
+			:equilateral
+		else
+			:isosceles
+		end
+	end
+
+	def valid?
+		sides = [x,y,z]
+		side_totals = [(x+y>z), (x+z>y), (y+z>x)]
+		raise TriangleError if sides.any? { |int| int <= 0 }
+		raise TriangleError if side_totals.any? { |x| x == false }
+ 	end
+
+	class TriangleError < StandardError
+	end
 end
